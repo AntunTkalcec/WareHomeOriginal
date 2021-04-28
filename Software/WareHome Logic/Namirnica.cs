f2971f3f2186a36ab751cb3c38bc777dd6a8602c
AntunTkalcec <antun22596@gmail.com>
2021-04-28 22:53:35 +0200

    Stvorena klasa namirnica

---
+++ Software/WareHome Logic/Namirnica.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHome_Logic
+{
+    public class Namirnica
+    {
+        public string NazivNamirnice { get; set; }
+        public int DostupnaKolicina { get; set; }
+        public int OptimalnaKolicina { get; set; }
+        public string Cijena { get; set; }
+        public string Ducan { get; set; }
+        public DateTime DatumKreiranja { get; set; }
+        public DateTime DatumZadnjePromjene { get; set; }
+        public string KorisnikKreator { get; set; }
+        public Domacinstvo Domacinstvo { get; set; }
+
+        public Namirnica(string nazivNamirnice, int dostupnaKolicina, int optimalnaKolicina, string cijena, string ducan, DateTime datumKreiranja, 
+            DateTime datumZadnjePromjene, string korisnikKreator, Domacinstvo domacinstvo)
+        {
+            NazivNamirnice = nazivNamirnice;
+            DostupnaKolicina = dostupnaKolicina;
+            OptimalnaKolicina = optimalnaKolicina;
+            Cijena = cijena;
+            Ducan = ducan;
+            DatumKreiranja = datumKreiranja;
+            DatumZadnjePromjene = datumZadnjePromjene;
+            KorisnikKreator = korisnikKreator;
+            Domacinstvo = domacinstvo;
+        }
+    }
+}
