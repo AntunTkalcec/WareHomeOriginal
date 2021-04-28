f0ab033a83e8937c80ecfd6cc48f90caf9d91767
AntunTkalcec <antun22596@gmail.com>
2021-04-28 20:32:21 +0200

    Merge branch 'master' of https://github.com/foivz/pi21-atkalcec-khabdija-kstrucic
    # Conflicts:
    #	Software/WareHome/WareHome.csproj

---
+++ Software/WareHome Logic/Korisnik.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHome_Logic
+{
+    public class Korisnik
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Mail { get; set; }
+        public string Lozinka { get; set; }
+        public string KorisnickoIme { get; set; }
+        public DateTime DatumRegistracije { get; set; }
+        public DateTime DatumZadnjePrijave { get; set; }
+        public Domacinstvo Domacinstvo { get; set; }
+        public bool Prijavljen { get; set; }
+
+        public Korisnik(string ime, string prezime, string mail, string lozinka, string korisnickoIme, DateTime datumRegistracije, DateTime datumZadnjePrijave, Domacinstvo domacinstvo)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            Mail = mail;
+            Lozinka = lozinka;
+            KorisnickoIme = korisnickoIme;
+            DatumRegistracije = datumRegistracije;
+            DatumZadnjePrijave = datumZadnjePrijave;
+            Domacinstvo = domacinstvo;
+        }
+    }
+}
---
+++ Software/WareHome/ZaboravljenaLozinkaForm.cs
@@ -0,0 +1,74 @@
+﻿using DatabaseAccess;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WareHome
+{
+    public partial class ZaboravljenaLozinkaForm : Form
+    {
+        public ZaboravljenaLozinkaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PovratiLozinkuButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                Database.Instance.Connect();
+                string sql = "SELECT * FROM Korisnik";
+                IDataReader dataReader = Database.Instance.GetDataReader(sql);
+                string email = "";
+                string lozinka = "";
+                bool pronadeno = false;
+                while (dataReader.Read())
+                {
+                    if (dataReader["korisnicko_ime"].ToString() == korimeZaboravljenoTextBox.Text)
+                    {
+                        email = dataReader["e-mail"].ToString();
+                        lozinka = dataReader["lozinka"].ToString();
+                        pronadeno = true;
+                    }
+                }
+                if (!pronadeno)
+                {
+                    MessageBox.Show("Ne postoji korisnik s tim korisničkim imenom.");
+                    dataReader.Close();
+                    Database.Instance.Disconnect();
+                    return;
+                }
+                mail.From = new MailAddress("warehomeapp@gmail.com");
+                mail.To.Add(email);
+                mail.Subject = "Povrat zaboravljene lozinke";
+                mail.Body = $"Vaša zaboravljena lozinka za WareHome aplikaciju jest: {lozinka}";
+
+                SmtpServer.Port = 587;
+                SmtpServer.UseDefaultCredentials = false;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("warehomeapp", "WarehomeApp246.");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                MessageBox.Show("Lozinka vam je poslana na mail na koji je registriran vaš korisnički račun.");
+                dataReader.Close();
+                Database.Instance.Disconnect();
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                Close();
+            }
+        }
+    }
+}
