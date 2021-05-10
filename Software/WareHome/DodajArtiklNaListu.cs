301c4c7c6b72a04d15a19d283d43657b197e766d
Karlo Habdija <khabdija@foi.hr>
2021-05-10 10:04:29 +0200

    DodajArtiklNaListu.cs - omogućeno dodavanje novog artikla na otvoreni popis za kupovinu - klikom na Dodaj artikl se dodaje korištenjem repozitorija NamirnicaNaListiRepository

--- Software/WareHome/DodajArtiklNaListu.cs
+++ Software/WareHome/DodajArtiklNaListu.cs
@@ -7,18 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WareHome.Models.ListaZaKupovinu;
 
 namespace WareHome
 {
     public partial class DodajArtiklNaListu : Form
     {
-        public DodajArtiklNaListu()
+        public ListaZaKupovinu odabranaLista;
+
+        public DodajArtiklNaListu(ListaZaKupovinu odabrana)
         {
             InitializeComponent();
+            odabranaLista = odabrana;
         }
 
         private void odustaniButton_Click(object sender, EventArgs e)
         {
+            Close();
+        }
+
+        private void dodajButton_Click(object sender, EventArgs e)
+        {
+            string nazivListe = odabranaLista.NazivListe;
+            string nazivArtikla = nazivTextBox.Text;
+            string cijena = cijenaTextBox.Text;
+            string količina = kolicinaTextBox.Text;
+            string trgovina = trgovinaTextBox.Text;
+            NamirnicaNaListi novaNamirnica = new NamirnicaNaListi(nazivListe, nazivArtikla, cijena, količina, trgovina);
+            NamirnicaNaListiRepository.DodajNamirnicu(novaNamirnica);
             Close();
         }
     }
