f46a560c8d2267ef21bd04d3e96443aaac45e98e
Karlo Habdija <khabdija@foi.hr>
2021-05-10 21:13:34 +0200

    ListaZaKupovinuRepository.cs - implementacija rada s bazom podataka - pobrisani dijelovi koda koji su se odnosili na unos testnih podataka - PreimenujListu se sada nalazi u ovom repozitoriju

--- Software/WareHome/Models/ListaZaKupovinu/ListaZaKupovinuRepository.cs
+++ Software/WareHome/Models/ListaZaKupovinu/ListaZaKupovinuRepository.cs
@@ -3,26 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WareHome_Logic;
+using DatabaseAccess;
+using System.Data;
 
 namespace WareHome.Models.ListaZaKupovinu
 {
     public static class ListaZaKupovinuRepository
     {
         public static List<ListaZaKupovinu> PopisLista;
+        public static Korisnik trenutniKorisnik;
+        public static string NoviNaziv;
 
         private static void IspuniPopisLista()
-        {
+        { 
             PopisLista = new List<ListaZaKupovinu>();
-            PopisLista.Add(new ListaZaKupovinu("KTC", false, null));
-            PopisLista.Add(new ListaZaKupovinu("Zabava u petak", true, "123"));
+            Database.Instance.Connect();
+            string sql = "SELECT * FROM Lista_za_kupovinu WHERE Lista_za_kupovinu.domacinstvo_id = " + trenutniKorisnik.Domacinstvo.Identifikator;
+            IDataReader dataReader = Database.Instance.GetDataReader(sql);
+            while (dataReader.Read())
+            {
+                if (dataReader != null)
+                {
+                    int id = int.Parse(dataReader["lista_id"].ToString());
+                    string naziv = dataReader["naziv_liste"].ToString();
+                    int privat = int.Parse(dataReader["privatna_lista"].ToString());
+                    bool privatna;
+                    string lozinka = dataReader["lozinka_liste"].ToString();
+                    int domacinstvo = int.Parse(dataReader["domacinstvo_id"].ToString());
+                    if (privat == 0)
+                    {
+                        privatna = false;
+                    }
+                    else
+                    {
+                        privatna = true;
+                    }
+                    ListaZaKupovinu listaZaKupovinu = new ListaZaKupovinu(id, naziv, privatna, lozinka, domacinstvo);
+                    PopisLista.Add(listaZaKupovinu);
+                }
+            }
         }
 
-        public static List<ListaZaKupovinu> DohvatiPopisLista()
+        public static List<ListaZaKupovinu> DohvatiPopisLista(Korisnik korisnik)
         {
-            if (PopisLista == null)
-            {
-                IspuniPopisLista();
-            }
+            trenutniKorisnik = null;
+            trenutniKorisnik = korisnik;
+            PopisLista = null;
+            IspuniPopisLista();
             return PopisLista;
         }
 
@@ -39,7 +67,18 @@ namespace WareHome.Models.ListaZaKupovinu
 
             if (!duplikat)
             {
-                PopisLista.Add(new ListaZaKupovinu(naziv, privatna, lozinka));
+                int privatnaInt = 0;
+                if (privatna)
+                {
+                    privatnaInt = 1;
+                }
+                Database.Instance.Connect();
+                string sql = "INSERT INTO Lista_za_kupovinu (naziv_liste, privatna_lista, lozinka_liste, domacinstvo_id) VALUES " +
+                    $"('{naziv}', {privatnaInt}, '{lozinka}', {trenutniKorisnik.Domacinstvo.Identifikator})";
+                Database.Instance.ExecuteCommand(sql);
+                Database.Instance.Disconnect();
+                PopisLista = null;
+                IspuniPopisLista();
             }
             else
             {
@@ -49,16 +88,30 @@ namespace WareHome.Models.ListaZaKupovinu
         
         public static void UkloniListu(ListaZaKupovinu ukloniListu)
         {
-            List<ListaZaKupovinu> filtrirano = new List<ListaZaKupovinu>();
-            foreach (var item in PopisLista)
+            Database.Instance.Connect();
+            string sql1 = "DELETE FROM Namirnica_na_listi WHERE lista_id = " + ukloniListu.IdListe;
+            Database.Instance.ExecuteCommand(sql1);
+            string sql2 = "DELETE FROM Lista_za_kupovinu WHERE lista_id = " + ukloniListu.IdListe;
+            Database.Instance.ExecuteCommand(sql2);
+            Database.Instance.Disconnect();
+            PopisLista = null;
+            IspuniPopisLista();
+        }
+
+        public static void PreimenujListu(ListaZaKupovinu preimenujListu, string naziv, bool privatna, string lozinka)
+        {
+            int privatnaInt = 0;
+            if (privatna)
             {
-                if (item.NazivListe.ToLower() != ukloniListu.NazivListe.ToLower())
-                {
-                    filtrirano.Add(item);
-                }
+                privatnaInt = 1;
             }
-            PopisLista.Clear();
-            PopisLista = filtrirano;
+            Database.Instance.Connect();
+            string sql = $"UPDATE Lista_za_kupovinu SET naziv_liste = '{naziv}', privatna_lista = {privatnaInt}, lozinka_liste = '{lozinka}' WHERE lista_id = {preimenujListu.IdListe}";
+            Database.Instance.ExecuteCommand(sql);
+            Database.Instance.Disconnect();
+            PopisLista = null;
+            IspuniPopisLista();
+            NoviNaziv = naziv;
         }
     }
 }
