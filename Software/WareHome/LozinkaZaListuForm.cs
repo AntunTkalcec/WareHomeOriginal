71914c637720b0818cc6f36ca5d150939458e76f
Karlo Habdija <khabdija@foi.hr>
2021-05-10 02:10:05 +0200

    LozinkaZaListuForm.cs - izrada forme za unos lozinke pri otvaranju privatne liste - korisnik ima 3 pokušaja da unese točnu lozinku, u suprotnom se prozor za unos zatvara - ako korisnik unese točnu lozinku, otvara se ListaForm

---
+++ Software/WareHome/LozinkaZaListuForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WareHome.Models.ListaZaKupovinu;
+
+namespace WareHome
+{
+    public partial class LozinkaZaListuForm : Form
+    {
+        ListaZaKupovinu odabranaLista;
+        int pokusaji = 3;
+
+        public LozinkaZaListuForm(ListaZaKupovinu odabrana)
+        {
+            InitializeComponent();
+            odabranaLista = odabrana;
+        }
+
+        private void odustaniButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void LozinkaZaListuForm_Load(object sender, EventArgs e)
+        {
+            lozinkaTextBox.PasswordChar = '*';
+            label1.Text = odabranaLista.NazivListe;
+        }
+
+        private void prikažiButton_Click(object sender, EventArgs e)
+        {
+            if (odabranaLista.LozinkaListe == lozinkaTextBox.Text)
+            {
+                ListaForm listaForm = new ListaForm(odabranaLista);
+                listaForm.Show();
+                Close();
+            }
+            else
+            {
+                if (pokusaji == 0)
+                {
+                    Close();
+                }
+                MessageBox.Show("Pogrešna lozinka! Preostalo pokušaja: " + pokusaji);
+                pokusaji--;
+            }
+        }
+    }
+}
