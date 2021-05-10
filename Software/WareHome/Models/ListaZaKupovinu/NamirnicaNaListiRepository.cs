d753208b85d3dad9bfd766b7ccfd7ee0b4e686b9
Karlo Habdija <khabdija@foi.hr>
2021-05-10 09:57:43 +0200

    NamirnicaNaListiRepository.cs - izrada repozitorija za upravljanje odabranom listom za trgovinu - IspuniPopis - trenutno ispunjava popise sa testnim podatcima, u daljnjim implementacijama će dohvaćati podatke iz baze - DohvatiPopisNamirnica - ispunjava popis upisanim namirnicama i vraća filtrirani popis koji se odnosi samo na odabranu listu - DodajNamirnicu - dodaje unesenu namirnicu na listu - UkloniNamirnicu - uklanja odabranu namirnicu s liste

---
+++ Software/WareHome/Models/ListaZaKupovinu/NamirnicaNaListiRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHome.Models.ListaZaKupovinu
+{
+    public static class NamirnicaNaListiRepository
+    {
+        public static List<NamirnicaNaListi> PopisNamirnica;
+        private static void IspuniPopis()
+        {
+            PopisNamirnica = new List<NamirnicaNaListi>();
+            PopisNamirnica.Add(new NamirnicaNaListi("KTC", "Kruh", "1", "", "KTC"));
+            PopisNamirnica.Add(new NamirnicaNaListi("KTC", "Mlijeko", "6", "7,99", ""));
+            PopisNamirnica.Add(new NamirnicaNaListi("Zabava u petak", "Jack Daniels 10l", "1", "1499", "KTC"));
+            PopisNamirnica.Add(new NamirnicaNaListi("Zabava u petak", "Karlovačko", "20", "8", "KTC"));
+            PopisNamirnica.Add(new NamirnicaNaListi("KTC", "Kuhalo za vodu", "", "", ""));
+        }
+
+        public static List<NamirnicaNaListi> DohvatiPopisNamirnica(ListaZaKupovinu odabranaLista)
+        {
+            if (PopisNamirnica == null)
+            {
+                IspuniPopis();
+            }
+
+            List<NamirnicaNaListi> filtrirano = new List<NamirnicaNaListi>();
+
+            foreach (var item in PopisNamirnica)
+            {
+                if (item.ListaNamirnice == odabranaLista.NazivListe)
+                {
+                    filtrirano.Add(item);
+                }
+            }
+
+            return filtrirano;
+        }
+
+        public static void DodajNamirnicu(NamirnicaNaListi dodajNamirnicu)
+        {
+            PopisNamirnica.Add(dodajNamirnicu);
+        }
+
+        public static void UkloniNamirnicu(NamirnicaNaListi ukloniNamirnicu)
+        {
+            PopisNamirnica.Remove(ukloniNamirnicu);
+        }
+    }
+}
