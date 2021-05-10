0de0df993526eadb4385280830b09de44440bc4f
Karlo Habdija <khabdija@foi.hr>
2021-05-10 09:58:32 +0200

    NamirnicaNaListu.cs - dodavanje identifikatora ListaNamirnice radi prepoznavanja koja namirnica se nalazi na kojoj listi

--- Software/WareHome/Models/ListaZaKupovinu/NamirnicaNaListi.cs
+++ Software/WareHome/Models/ListaZaKupovinu/NamirnicaNaListi.cs
@@ -8,12 +8,14 @@ namespace WareHome.Models.ListaZaKupovinu
 {
     public class NamirnicaNaListi
     {
+        public string ListaNamirnice { get; set; }
         public string NazivNamirnice { get; set; }
         public string KoličinaNamirnice { get; set; }
         public string CijenaNamirnice { get; set; }
         public string TrgovinaNamirnice { get; set; }
-        public NamirnicaNaListi(string naziv, string kolicina, string cijena, string trgovina)
+        public NamirnicaNaListi(string lista, string naziv, string kolicina, string cijena, string trgovina)
         {
+            ListaNamirnice = lista;
             NazivNamirnice = naziv;
             KoličinaNamirnice = kolicina;
             CijenaNamirnice = cijena;
