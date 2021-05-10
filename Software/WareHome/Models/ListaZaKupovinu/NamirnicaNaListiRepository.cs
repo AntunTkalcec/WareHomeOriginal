034d20528416645c502fbf57f6db9ac6d4047215
Karlo Habdija <khabdija@foi.hr>
2021-05-10 21:14:28 +0200

    NamirnicaNaListiRepository.cs - implementacija rada s bazom podataka - pobrisan dio koda koji se odnosio na unos testnih podataka

--- Software/WareHome/Models/ListaZaKupovinu/NamirnicaNaListiRepository.cs
+++ Software/WareHome/Models/ListaZaKupovinu/NamirnicaNaListiRepository.cs
@@ -1,52 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DatabaseAccess;
 
 namespace WareHome.Models.ListaZaKupovinu
 {
     public static class NamirnicaNaListiRepository
     {
         public static List<NamirnicaNaListi> PopisNamirnica;
+        static ListaZaKupovinu listaZaKupovinu;
         private static void IspuniPopis()
         {
             PopisNamirnica = new List<NamirnicaNaListi>();
-            PopisNamirnica.Add(new NamirnicaNaListi("KTC", "Kruh", "1", "", "KTC"));
-            PopisNamirnica.Add(new NamirnicaNaListi("KTC", "Mlijeko", "6", "7,99", ""));
-            PopisNamirnica.Add(new NamirnicaNaListi("Zabava u petak", "Jack Daniels 10l", "1", "1499", "KTC"));
-            PopisNamirnica.Add(new NamirnicaNaListi("Zabava u petak", "Karlovačko", "20", "8", "KTC"));
-            PopisNamirnica.Add(new NamirnicaNaListi("KTC", "Kuhalo za vodu", "", "", ""));
+            Database.Instance.Connect();
+            string sql = "SELECT * FROM Namirnica_na_listi WHERE Namirnica_na_listi.lista_id = " + listaZaKupovinu.IdListe;
+            IDataReader dataReader = Database.Instance.GetDataReader(sql);
+            while (dataReader.Read())
+            {
+                if (dataReader != null)
+                {
+                    int id = int.Parse(dataReader["namirnicaNaListi_id"].ToString());
+                    string naziv = dataReader["naziv_namirniceNaListi"].ToString();
+                    string kolicina = dataReader["kolicina_namirniceNaListi"].ToString();
+                    string cijena = dataReader["cijena_namirniceNaListi"].ToString();
+                    string trgovina = dataReader["trgovina_namirniceNaListi"].ToString();
+                   
+                    NamirnicaNaListi namirnica = new NamirnicaNaListi(id, listaZaKupovinu.IdListe, naziv, kolicina, cijena, trgovina);
+                    PopisNamirnica.Add(namirnica);
+                }
+            }
+            Database.Instance.Disconnect();
         }
 
         public static List<NamirnicaNaListi> DohvatiPopisNamirnica(ListaZaKupovinu odabranaLista)
         {
+            listaZaKupovinu = odabranaLista;
+
             if (PopisNamirnica == null)
             {
                 IspuniPopis();
             }
 
-            List<NamirnicaNaListi> filtrirano = new List<NamirnicaNaListi>();
-
-            foreach (var item in PopisNamirnica)
-            {
-                if (item.ListaNamirnice == odabranaLista.NazivListe)
-                {
-                    filtrirano.Add(item);
-                }
-            }
-
-            return filtrirano;
+            return PopisNamirnica;
         }
 
         public static void DodajNamirnicu(NamirnicaNaListi dodajNamirnicu)
         {
-            PopisNamirnica.Add(dodajNamirnicu);
+            Database.Instance.Connect();
+            string sql = "INSERT INTO Namirnica_na_listi (lista_id, naziv_namirniceNaListi, kolicina_namirniceNaListi, cijena_namirniceNaListi, trgovina_namirniceNaListi) VALUES " +
+                $"({dodajNamirnicu.ListaNamirnice}, '{dodajNamirnicu.NazivNamirnice}', '{dodajNamirnicu.KoličinaNamirnice}', '{dodajNamirnicu.CijenaNamirnice}', '{dodajNamirnicu.TrgovinaNamirnice}')";
+            Database.Instance.ExecuteCommand(sql);
+            Database.Instance.Disconnect();
+            PopisNamirnica = null;
+            IspuniPopis();
         }
 
         public static void UkloniNamirnicu(NamirnicaNaListi ukloniNamirnicu)
         {
-            PopisNamirnica.Remove(ukloniNamirnicu);
+            Database.Instance.Connect();
+            string sql = "DELETE FROM Namirnica_na_listi WHERE namirnicaNaListi_id = " + ukloniNamirnicu.IdNamirnice;
+            Database.Instance.ExecuteCommand(sql);
+            Database.Instance.Disconnect();
+            PopisNamirnica = null;
+            IspuniPopis();
         }
     }
 }
