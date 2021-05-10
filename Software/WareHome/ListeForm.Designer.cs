d1d5841d952ac71da3b480db09ab3ae54690d24e
Karlo Habdija <khabdija@foi.hr>
2021-05-10 21:08:09 +0200

    ListeForm.cs - implementacija rada s bazom podataka - u parametar kod pokretanja forme dodan Korisnik - ako Korisnik nije član domaćinstva, nema mogućnost izrade liste - popravak sitnih bugova (klik na Obriši ili Prikaži bez da je red selektiran je izbacivao grešku)

--- Software/WareHome/ListeForm.Designer.cs
+++ Software/WareHome/ListeForm.Designer.cs
@@ -34,26 +34,25 @@ namespace WareHome
             this.prikažiButton = new System.Windows.Forms.Button();
             this.kreirajButton = new System.Windows.Forms.Button();
             this.obrišiButton = new System.Windows.Forms.Button();
+            this.nisteDioDomacinstvaLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.listeDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // listeDataGridView
             // 
             this.listeDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.listeDataGridView.Location = new System.Drawing.Point(16, 15);
-            this.listeDataGridView.Margin = new System.Windows.Forms.Padding(4);
+            this.listeDataGridView.Location = new System.Drawing.Point(12, 12);
             this.listeDataGridView.Name = "listeDataGridView";
             this.listeDataGridView.RowHeadersWidth = 51;
             this.listeDataGridView.ShowEditingIcon = false;
-            this.listeDataGridView.Size = new System.Drawing.Size(636, 320);
+            this.listeDataGridView.Size = new System.Drawing.Size(477, 260);
             this.listeDataGridView.TabIndex = 0;
             // 
             // povratakButton
             // 
-            this.povratakButton.Location = new System.Drawing.Point(16, 342);
-            this.povratakButton.Margin = new System.Windows.Forms.Padding(4);
+            this.povratakButton.Location = new System.Drawing.Point(12, 278);
             this.povratakButton.Name = "povratakButton";
-            this.povratakButton.Size = new System.Drawing.Size(127, 44);
+            this.povratakButton.Size = new System.Drawing.Size(95, 36);
             this.povratakButton.TabIndex = 1;
             this.povratakButton.Text = "Povratak";
             this.povratakButton.UseVisualStyleBackColor = true;
@@ -61,10 +60,9 @@ namespace WareHome
             // 
             // prikažiButton
             // 
-            this.prikažiButton.Location = new System.Drawing.Point(525, 342);
-            this.prikažiButton.Margin = new System.Windows.Forms.Padding(4);
+            this.prikažiButton.Location = new System.Drawing.Point(394, 278);
             this.prikažiButton.Name = "prikažiButton";
-            this.prikažiButton.Size = new System.Drawing.Size(127, 44);
+            this.prikažiButton.Size = new System.Drawing.Size(95, 36);
             this.prikažiButton.TabIndex = 4;
             this.prikažiButton.Text = "Prikaži";
             this.prikažiButton.UseVisualStyleBackColor = true;
@@ -72,10 +70,9 @@ namespace WareHome
             // 
             // kreirajButton
             // 
-            this.kreirajButton.Location = new System.Drawing.Point(391, 342);
-            this.kreirajButton.Margin = new System.Windows.Forms.Padding(4);
+            this.kreirajButton.Location = new System.Drawing.Point(293, 278);
             this.kreirajButton.Name = "kreirajButton";
-            this.kreirajButton.Size = new System.Drawing.Size(127, 44);
+            this.kreirajButton.Size = new System.Drawing.Size(95, 36);
             this.kreirajButton.TabIndex = 3;
             this.kreirajButton.Text = "Kreiraj";
             this.kreirajButton.UseVisualStyleBackColor = true;
@@ -83,31 +80,45 @@ namespace WareHome
             // 
             // obrišiButton
             // 
-            this.obrišiButton.Location = new System.Drawing.Point(256, 342);
-            this.obrišiButton.Margin = new System.Windows.Forms.Padding(4);
+            this.obrišiButton.Location = new System.Drawing.Point(192, 278);
             this.obrišiButton.Name = "obrišiButton";
-            this.obrišiButton.Size = new System.Drawing.Size(127, 44);
+            this.obrišiButton.Size = new System.Drawing.Size(95, 36);
             this.obrišiButton.TabIndex = 2;
             this.obrišiButton.Text = "Obriši";
             this.obrišiButton.UseVisualStyleBackColor = true;
             this.obrišiButton.Click += new System.EventHandler(this.obrišiButton_Click);
             // 
+            // nisteDioDomacinstvaLabel
+            // 
+            this.nisteDioDomacinstvaLabel.AutoSize = true;
+            this.nisteDioDomacinstvaLabel.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.nisteDioDomacinstvaLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nisteDioDomacinstvaLabel.Font = new System.Drawing.Font("Century Gothic", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nisteDioDomacinstvaLabel.ForeColor = System.Drawing.Color.Red;
+            this.nisteDioDomacinstvaLabel.Location = new System.Drawing.Point(95, 130);
+            this.nisteDioDomacinstvaLabel.Name = "nisteDioDomacinstvaLabel";
+            this.nisteDioDomacinstvaLabel.Size = new System.Drawing.Size(313, 33);
+            this.nisteDioDomacinstvaLabel.TabIndex = 25;
+            this.nisteDioDomacinstvaLabel.Text = "Niste dio domaćinstva.";
+            this.nisteDioDomacinstvaLabel.Visible = false;
+            // 
             // ListeForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(688, 411);
+            this.ClientSize = new System.Drawing.Size(516, 334);
+            this.Controls.Add(this.nisteDioDomacinstvaLabel);
             this.Controls.Add(this.obrišiButton);
             this.Controls.Add(this.kreirajButton);
             this.Controls.Add(this.prikažiButton);
             this.Controls.Add(this.povratakButton);
             this.Controls.Add(this.listeDataGridView);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ListeForm";
             this.Text = "WareHome";
             this.Load += new System.EventHandler(this.ListeForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.listeDataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -118,5 +129,6 @@ namespace WareHome
         private System.Windows.Forms.Button prikažiButton;
         private System.Windows.Forms.Button kreirajButton;
         private System.Windows.Forms.Button obrišiButton;
+        private System.Windows.Forms.Label nisteDioDomacinstvaLabel;
     }
 }
--- Software/WareHome/ListeForm.cs
+++ Software/WareHome/ListeForm.cs
@@ -8,42 +8,71 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WareHome.Models.ListaZaKupovinu;
+using WareHome_Logic;
 
 namespace WareHome
 {
     public partial class ListeForm : Form
     {
         List<ListaZaKupovinu> listeZaKupovinu;
+        Korisnik trenutniKorisnik;
 
-        public ListeForm()
+        public ListeForm(Korisnik korisnik)
         {
             InitializeComponent();
+            trenutniKorisnik = korisnik;
         }
 
         private void ListeForm_Load(object sender, EventArgs e)
         {
-            OsvjeziListu();
+            if (trenutniKorisnik.Domacinstvo == null)
+            {
+                KorisnikNemaDomacinstvo();
+            }
+            else
+            {
+                OsvjeziListu();
+            }
+        }
+
+        private void KorisnikNemaDomacinstvo()
+        {
+            listeDataGridView.DataSource = null;
+            kreirajButton.Enabled = false;
+            obrišiButton.Enabled = false;
+            prikažiButton.Enabled = false;
+            nisteDioDomacinstvaLabel.Visible = true;
         }
 
         private void povratakButton_Click(object sender, EventArgs e)
         {
+            trenutniKorisnik = null;
             Close();
         }
 
         private void prikažiButton_Click(object sender, EventArgs e)
         {
-            ListaZaKupovinu odabrana = listeDataGridView.CurrentRow.DataBoundItem as ListaZaKupovinu;
-            if (odabrana.PrivatnaLista)
+            if (listeDataGridView.CurrentRow == null)
             {
-                LozinkaZaListuForm lozinkaZaListuForm = new LozinkaZaListuForm(odabrana);
-                lozinkaZaListuForm.ShowDialog();
+                OsvjeziListu();
             }
             else
             {
-                ListaForm listaForm = new ListaForm(odabrana);
-                listaForm.Show();
+                ListaZaKupovinu odabrana = listeDataGridView.CurrentRow.DataBoundItem as ListaZaKupovinu;
+
+                if (odabrana.PrivatnaLista)
+                {
+                    LozinkaZaListuForm lozinkaZaListuForm = new LozinkaZaListuForm(odabrana);
+                    lozinkaZaListuForm.ShowDialog();
+                    OsvjeziListu();
+                }
+                else
+                {
+                    ListaForm listaForm = new ListaForm(odabrana);
+                    listaForm.Show();
+                    OsvjeziListu();
+                }
             }
-            OsvjeziListu();
         }
 
         private void kreirajButton_Click(object sender, EventArgs e)
@@ -56,22 +85,32 @@ namespace WareHome
         private void OsvjeziListu()
         {
             listeZaKupovinu = null;
-            listeZaKupovinu = ListaZaKupovinuRepository.DohvatiPopisLista();
+            listeZaKupovinu = ListaZaKupovinuRepository.DohvatiPopisLista(trenutniKorisnik);
             listeDataGridView.DataSource = null;
             listeDataGridView.DataSource = listeZaKupovinu;
             listeDataGridView.Columns["LozinkaListe"].Visible = false;
+            listeDataGridView.Columns["IdListe"].Visible = false;
+            listeDataGridView.Columns["Domacinstvo"].Visible = false;
             listeDataGridView.Columns["PrivatnaLista"].ReadOnly = true;
             listeDataGridView.Columns["NazivListe"].ReadOnly = true;
             listeDataGridView.Columns["PrivatnaLista"].HeaderText = "Privatna lista";
             listeDataGridView.Columns["NazivListe"].HeaderText = "Naziv liste";
+            nisteDioDomacinstvaLabel.Visible = false;
+            NamirnicaNaListiRepository.PopisNamirnica = null;
         }
 
         private void obrišiButton_Click(object sender, EventArgs e)
         {
-            ListaZaKupovinu obriši = listeDataGridView.CurrentRow.DataBoundItem as ListaZaKupovinu;
-            ListaZaKupovinuRepository.UkloniListu(obriši);
-
-            OsvjeziListu();
+            if (listeDataGridView.CurrentRow == null)
+            {
+                OsvjeziListu();
+            }                        
+            else
+            {
+                ListaZaKupovinu obriši = listeDataGridView.CurrentRow.DataBoundItem as ListaZaKupovinu;
+                ListaZaKupovinuRepository.UkloniListu(obriši);
+                OsvjeziListu();
+            }
         }
     }
 }
