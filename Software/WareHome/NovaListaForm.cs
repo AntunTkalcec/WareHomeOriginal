390ab0120e5d29acd3774fa076716ecbef8e116f
Karlo Habdija <khabdija@foi.hr>
2021-05-10 02:05:37 +0200

    NovaListaForm.cs - implementacija repozitorija ListaZaKupovinuRepository koji se koristi za sve radnje na formi - svi gumbi su funkcionalni - ukoliko je polje za unos naziva prazno, program ispisuje grešku

--- Software/WareHome/NovaListaForm.cs
+++ Software/WareHome/NovaListaForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WareHome.Models.ListaZaKupovinu;
 
 namespace WareHome
 {
@@ -19,18 +20,53 @@ namespace WareHome
 
         private void NovaListaForm_Load(object sender, EventArgs e)
         {
-
+            privatnaCheckBox.Checked = false;
+            lozinkaTextBox.Enabled = false;
+            lozinkaTextBox.PasswordChar = '*';
         }
 
         private void završiButton_Click(object sender, EventArgs e)
         {
+            string naziv = nazivTextBox.Text;
+            bool privatna;
+            string lozinka;
+            if (privatnaCheckBox.Checked)
+            {
+                privatna = true;
+                lozinka = lozinkaTextBox.Text;
+            }
+            else
+            {
+                privatna = false;
+                lozinka = null;
+            }
 
-            Close();
+            if (naziv != "")
+            {
+                ListaZaKupovinuRepository.DodajListu(naziv, privatna, lozinka);
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Unesite naziv liste!");
+            }
         }
 
         private void odustaniButton_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void privatnaCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (privatnaCheckBox.Checked)
+            {
+                lozinkaTextBox.Enabled = true;
+            }
+            else
+            {
+                lozinkaTextBox.Enabled = false;
+            }
         }
     }
 }
