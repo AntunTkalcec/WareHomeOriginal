7765272e5a1a240c4c6725c7c8d2fc213c5c1870
Karlo Habdija <khabdija@foi.hr>
2021-05-11 17:44:17 +0200

    - klasa ListeZaKupovinu preimenovana u ListaZaKupovinu - klasa NamirniceNaListi preimenovana u NamirnicaNaListi - u obje klase dodani identifikatori koji se koriste u bazi podataka

--- Software/WareHome/Models/ListaZaKupovinu/ListaZaKupovinu.cs
+++ Software/WareHome/Models/ListaZaKupovinu/ListaZaKupovinu.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WareHome_Logic;
 
 namespace WareHome.Models.ListaZaKupovinu
 {
-    public class ListeZaKupovinu
+    public class ListaZaKupovinu
     {
+        public int IdListe { get; set; }
         public string NazivListe { get; set; }
         public bool PrivatnaLista { get; set; }
         public string LozinkaListe { get; set; }
-        public ListeZaKupovinu(string naziv, bool privatna, string lozinka)
+        public int Domacinstvo { get; set; }
+        public ListaZaKupovinu(int id, string naziv, bool privatna, string lozinka, int domacinstvo)
         {
+            IdListe = id;
             NazivListe = naziv;
             PrivatnaLista = privatna;
             LozinkaListe = lozinka;
+            Domacinstvo = domacinstvo;
         }
     }
 }
--- Software/WareHome/Models/ListaZaKupovinu/NamirnicaNaListi.cs
+++ Software/WareHome/Models/ListaZaKupovinu/NamirnicaNaListi.cs
@@ -6,14 +6,18 @@ using System.Threading.Tasks;
 
 namespace WareHome.Models.ListaZaKupovinu
 {
-    public class NamirniceNaListi
+    public class NamirnicaNaListi
     {
+        public int IdNamirnice { get; set; }
+        public int ListaNamirnice { get; set; }
         public string NazivNamirnice { get; set; }
         public string KoličinaNamirnice { get; set; }
         public string CijenaNamirnice { get; set; }
         public string TrgovinaNamirnice { get; set; }
-        public NamirniceNaListi(string naziv, string kolicina, string cijena, string trgovina)
+        public NamirnicaNaListi(int id, int lista, string naziv, string kolicina, string cijena, string trgovina)
         {
+            IdNamirnice = id;
+            ListaNamirnice = lista;
             NazivNamirnice = naziv;
             KoličinaNamirnice = kolicina;
             CijenaNamirnice = cijena;
