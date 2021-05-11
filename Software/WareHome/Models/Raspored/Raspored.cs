1114fd2ebb3e5eb9f175c1093d2c02f50480fbc6
Karlo Habdija <khabdija@foi.hr>
2021-05-11 18:01:05 +0200

    Raspored.cs - brisanje konstruktora - dodavanje atributa potrebnih za rad s bazom podataka

--- Software/WareHome/Models/Raspored/Raspored.cs
+++ Software/WareHome/Models/Raspored/Raspored.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WareHome_Logic;
 
 namespace WareHome.Models.Raspored
 {
     public class Raspored
     {
+        public int RasporedId { get; set; }
         public Dan OdabraniDan { get; set; }
         public Sat OdabraniSat { get; set; }
         public Minuta OdabranaMinuta { get; set; }
         public Ponavljanje OdabirPonavljanja { get; set; }
+        public Korisnik Korisnik { get; set; }
         public Raspored()
         {
-            OdabraniDan = Dozvoljeno.Dani[0];
-            OdabraniSat = Dozvoljeno.Sati[0];
-            OdabranaMinuta = Dozvoljeno.Minute[0];
-            OdabirPonavljanja = Dozvoljeno.Ponavljanja[0];
         }
     }
 }
