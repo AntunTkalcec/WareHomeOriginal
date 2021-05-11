5e85e959973d82275140d54c34d22688619fa282
Karlo Habdija <khabdija@foi.hr>
2021-05-11 17:45:11 +0200

    DodajArtiklNaListu.cs - omogućeno dodavanje novog artikla na otvoreni popis za kupovinu - klikom na Dodaj artikl se dodaje korištenjem repozitorija NamirnicaNaListiRepository - minimalne preinake u dizajnu prozora

--- Software/WareHome/DodajArtiklNaListu.Designer.cs
+++ Software/WareHome/DodajArtiklNaListu.Designer.cs
@@ -44,91 +44,102 @@ namespace WareHome
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 16);
+            this.label1.Location = new System.Drawing.Point(15, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.Size = new System.Drawing.Size(47, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Naziv:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(11, 42);
+            this.label2.Location = new System.Drawing.Point(15, 52);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 13);
-            this.label2.TabIndex = 1;
+            this.label2.Size = new System.Drawing.Size(51, 17);
+            this.label2.TabIndex = 0;
             this.label2.Text = "Cijena:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(11, 94);
+            this.label3.Location = new System.Drawing.Point(15, 116);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 13);
-            this.label3.TabIndex = 3;
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 0;
             this.label3.Text = "Trgovina:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(11, 68);
+            this.label4.Location = new System.Drawing.Point(15, 84);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(47, 13);
-            this.label4.TabIndex = 2;
+            this.label4.Size = new System.Drawing.Size(61, 17);
+            this.label4.TabIndex = 0;
             this.label4.Text = "Količina:";
             // 
             // nazivTextBox
             // 
-            this.nazivTextBox.Location = new System.Drawing.Point(69, 13);
+            this.nazivTextBox.Location = new System.Drawing.Point(92, 16);
+            this.nazivTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.nazivTextBox.Name = "nazivTextBox";
-            this.nazivTextBox.Size = new System.Drawing.Size(134, 20);
-            this.nazivTextBox.TabIndex = 4;
+            this.nazivTextBox.Size = new System.Drawing.Size(177, 22);
+            this.nazivTextBox.TabIndex = 1;
             // 
             // cijenaTextBox
             // 
-            this.cijenaTextBox.Location = new System.Drawing.Point(69, 39);
+            this.cijenaTextBox.Location = new System.Drawing.Point(92, 48);
+            this.cijenaTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.cijenaTextBox.Name = "cijenaTextBox";
-            this.cijenaTextBox.Size = new System.Drawing.Size(134, 20);
-            this.cijenaTextBox.TabIndex = 5;
+            this.cijenaTextBox.Size = new System.Drawing.Size(177, 22);
+            this.cijenaTextBox.TabIndex = 2;
             // 
             // kolicinaTextBox
             // 
-            this.kolicinaTextBox.Location = new System.Drawing.Point(69, 65);
+            this.kolicinaTextBox.Location = new System.Drawing.Point(92, 80);
+            this.kolicinaTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.kolicinaTextBox.Name = "kolicinaTextBox";
-            this.kolicinaTextBox.Size = new System.Drawing.Size(59, 20);
-            this.kolicinaTextBox.TabIndex = 6;
+            this.kolicinaTextBox.Size = new System.Drawing.Size(77, 22);
+            this.kolicinaTextBox.TabIndex = 3;
             // 
             // trgovinaTextBox
             // 
-            this.trgovinaTextBox.Location = new System.Drawing.Point(69, 91);
+            this.trgovinaTextBox.Location = new System.Drawing.Point(92, 112);
+            this.trgovinaTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.trgovinaTextBox.Name = "trgovinaTextBox";
-            this.trgovinaTextBox.Size = new System.Drawing.Size(134, 20);
-            this.trgovinaTextBox.TabIndex = 7;
+            this.trgovinaTextBox.Size = new System.Drawing.Size(177, 22);
+            this.trgovinaTextBox.TabIndex = 4;
             // 
             // dodajButton
             // 
-            this.dodajButton.Location = new System.Drawing.Point(128, 127);
+            this.dodajButton.Location = new System.Drawing.Point(171, 156);
+            this.dodajButton.Margin = new System.Windows.Forms.Padding(4);
             this.dodajButton.Name = "dodajButton";
-            this.dodajButton.Size = new System.Drawing.Size(75, 23);
-            this.dodajButton.TabIndex = 8;
+            this.dodajButton.Size = new System.Drawing.Size(100, 28);
+            this.dodajButton.TabIndex = 5;
             this.dodajButton.Text = "Dodaj";
             this.dodajButton.UseVisualStyleBackColor = true;
+            this.dodajButton.Click += new System.EventHandler(this.dodajButton_Click);
             // 
             // odustaniButton
             // 
-            this.odustaniButton.Location = new System.Drawing.Point(14, 127);
+            this.odustaniButton.Location = new System.Drawing.Point(19, 156);
+            this.odustaniButton.Margin = new System.Windows.Forms.Padding(4);
             this.odustaniButton.Name = "odustaniButton";
-            this.odustaniButton.Size = new System.Drawing.Size(75, 23);
-            this.odustaniButton.TabIndex = 9;
+            this.odustaniButton.Size = new System.Drawing.Size(100, 28);
+            this.odustaniButton.TabIndex = 6;
             this.odustaniButton.Text = "Odustani";
             this.odustaniButton.UseVisualStyleBackColor = true;
             this.odustaniButton.Click += new System.EventHandler(this.odustaniButton_Click);
             // 
             // DodajArtiklNaListu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(237, 176);
+            this.ClientSize = new System.Drawing.Size(296, 205);
             this.Controls.Add(this.odustaniButton);
             this.Controls.Add(this.dodajButton);
             this.Controls.Add(this.trgovinaTextBox);
@@ -139,6 +150,7 @@ namespace WareHome
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "DodajArtiklNaListu";
             this.Text = "WareHome";
             this.ResumeLayout(false);
--- Software/WareHome/DodajArtiklNaListu.cs
+++ Software/WareHome/DodajArtiklNaListu.cs
@@ -7,18 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WareHome.Models.ListaZaKupovinu;
 
 namespace WareHome
 {
     public partial class DodajArtiklNaListu : Form
     {
-        public DodajArtiklNaListu()
+        public ListaZaKupovinu odabranaLista;
+
+        public DodajArtiklNaListu(ListaZaKupovinu odabrana)
         {
             InitializeComponent();
+            odabranaLista = odabrana;
         }
 
         private void odustaniButton_Click(object sender, EventArgs e)
         {
+            Close();
+        }
+
+        private void dodajButton_Click(object sender, EventArgs e)
+        {
+            string nazivArtikla = nazivTextBox.Text;
+            string cijena = cijenaTextBox.Text;
+            string količina = kolicinaTextBox.Text;
+            string trgovina = trgovinaTextBox.Text;
+            NamirnicaNaListiRepository.DodajNamirnicu(new NamirnicaNaListi(0, odabranaLista.IdListe, nazivArtikla, cijena, količina, trgovina));
             Close();
         }
     }
