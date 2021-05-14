f907b988e390b425edc683a48418e9e651e4b0a0
Karlo Habdija <khabdija@foi.hr>
2021-05-14 13:25:13 +0200

    Predviđanjepotrošnje.cs - izrada klase za upis potrošnje namirnica iz baze podataka

---
+++ Software/WareHome/Models/PredviđanjePotrošnje/PredviđenaPotrošnja.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHome.Models.PredviđanjePotrošnje
+{
+    public class PredviđenaPotrošnja
+    {
+        public int NamirnicaId { get; set; }
+        public string NamirnicaNaziv { get; set; }
+        public float DostupnaKoličina { get; set; }
+        public float PredviđenoTrajanje { get; set; }
+        public PredviđenaPotrošnja()
+        {
+
+        }
+    }
+}
