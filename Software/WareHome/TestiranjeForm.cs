3df3d0142b8b67ff5f4afd236a40a7271cad7749
Karlo Habdija <khabdija@foi.hr>
2021-05-16 10:24:04 +0200

    TestiranjeForm - uređivanje izgleda forme za testiranje radi lakše preglednosti (strukturirani readonly DGV)

--- Software/WareHome/TestiranjeForm.cs
+++ Software/WareHome/TestiranjeForm.cs
@@ -30,8 +30,30 @@ namespace WareHome
 
         private void TestiranjeForm_Load(object sender, EventArgs e)
         {
-            List<PredviđenaPotrošnja> predviđenaPotrošnja = PredviđanjePotrošnjeRepository.PredvidiPotrošnju(trenutniKorisnik);
+            List<PredviđenaPotrošnja> predviđenaPotrošnja = null;
+            predviđenaPotrošnja = PredviđanjePotrošnjeRepository.PredvidiPotrošnju(trenutniKorisnik);
             dataGridView1.DataSource = predviđenaPotrošnja;
+
+            dataGridView1.Columns["NamirnicaId"].ReadOnly = true;
+            dataGridView1.Columns["NamirnicaId"].HeaderText = "ID";
+            dataGridView1.Columns["NamirnicaId"].Width = 25;
+
+            dataGridView1.Columns["NamirnicaNaziv"].ReadOnly = true;
+            dataGridView1.Columns["NamirnicaNaziv"].HeaderText = "Naziv";
+            dataGridView1.Columns["NamirnicaNaziv"].Width = 221;
+
+            dataGridView1.Columns["DostupnaKoličina"].ReadOnly = true;
+            dataGridView1.Columns["DostupnaKoličina"].HeaderText = "Dostupna količina";
+            dataGridView1.Columns["DostupnaKoličina"].Width = 150;
+            
+            dataGridView1.Columns["MjernaJedinica"].ReadOnly = true;
+            dataGridView1.Columns["MjernaJedinica"].HeaderText = "Mjerna jedinica";
+            dataGridView1.Columns["MjernaJedinica"].Width = 150;
+
+            dataGridView1.Columns["PredviđenoTrajanje"].ReadOnly = true;
+            dataGridView1.Columns["PredviđenoTrajanje"].HeaderText = "Predviđeno trajanje (u danima)";
+            dataGridView1.Columns["PredviđenoTrajanje"].Width = 175;
+
         }
     }
 }
