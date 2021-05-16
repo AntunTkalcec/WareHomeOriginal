1f3bba4e8ec71c10c9754f68a0152de2f9a9ecb5
Karlo Habdija <khabdija@foi.hr>
2021-05-16 15:20:45 +0200

    RasporedForm - definiran CancelButton koji gasi formu klikom na escape - minimalne promjene u dizajnu forme

--- Software/WareHome/RasporedForm.Designer.cs
+++ Software/WareHome/RasporedForm.Designer.cs
@@ -49,26 +49,26 @@ namespace WareHome
             // danComboBox
             // 
             this.danComboBox.FormattingEnabled = true;
-            this.danComboBox.Location = new System.Drawing.Point(107, 62);
+            this.danComboBox.Location = new System.Drawing.Point(134, 62);
             this.danComboBox.Margin = new System.Windows.Forms.Padding(4);
             this.danComboBox.Name = "danComboBox";
-            this.danComboBox.Size = new System.Drawing.Size(191, 24);
+            this.danComboBox.Size = new System.Drawing.Size(164, 24);
             this.danComboBox.TabIndex = 2;
             this.danComboBox.SelectedIndexChanged += new System.EventHandler(this.danComboBox_SelectedIndexChanged);
             // 
             // ponavljanjeComboBox
             // 
             this.ponavljanjeComboBox.FormattingEnabled = true;
-            this.ponavljanjeComboBox.Location = new System.Drawing.Point(162, 128);
+            this.ponavljanjeComboBox.Location = new System.Drawing.Point(183, 128);
             this.ponavljanjeComboBox.Margin = new System.Windows.Forms.Padding(4);
             this.ponavljanjeComboBox.Name = "ponavljanjeComboBox";
-            this.ponavljanjeComboBox.Size = new System.Drawing.Size(65, 24);
+            this.ponavljanjeComboBox.Size = new System.Drawing.Size(54, 24);
             this.ponavljanjeComboBox.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(196, 98);
+            this.label1.Location = new System.Drawing.Point(211, 98);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(12, 17);
@@ -77,7 +77,7 @@ namespace WareHome
             // 
             // spremiButton
             // 
-            this.spremiButton.Location = new System.Drawing.Point(183, 175);
+            this.spremiButton.Location = new System.Drawing.Point(201, 175);
             this.spremiButton.Margin = new System.Windows.Forms.Padding(4);
             this.spremiButton.Name = "spremiButton";
             this.spremiButton.Size = new System.Drawing.Size(97, 42);
@@ -88,7 +88,7 @@ namespace WareHome
             // 
             // odustaniButton
             // 
-            this.odustaniButton.Location = new System.Drawing.Point(45, 175);
+            this.odustaniButton.Location = new System.Drawing.Point(29, 175);
             this.odustaniButton.Margin = new System.Windows.Forms.Padding(4);
             this.odustaniButton.Name = "odustaniButton";
             this.odustaniButton.Size = new System.Drawing.Size(97, 42);
@@ -101,7 +101,7 @@ namespace WareHome
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(13, 65);
+            this.label2.Location = new System.Drawing.Point(26, 65);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(42, 17);
@@ -112,7 +112,7 @@ namespace WareHome
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(13, 98);
+            this.label3.Location = new System.Drawing.Point(26, 98);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(67, 17);
@@ -123,7 +123,7 @@ namespace WareHome
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(13, 132);
+            this.label4.Location = new System.Drawing.Point(26, 132);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(97, 17);
@@ -133,7 +133,7 @@ namespace WareHome
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(112, 132);
+            this.label5.Location = new System.Drawing.Point(131, 132);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(48, 17);
@@ -143,7 +143,7 @@ namespace WareHome
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(229, 132);
+            this.label6.Location = new System.Drawing.Point(239, 132);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(59, 17);
@@ -153,25 +153,25 @@ namespace WareHome
             // satComboBox
             // 
             this.satComboBox.FormattingEnabled = true;
-            this.satComboBox.Location = new System.Drawing.Point(107, 95);
+            this.satComboBox.Location = new System.Drawing.Point(134, 95);
             this.satComboBox.Margin = new System.Windows.Forms.Padding(4);
             this.satComboBox.Name = "satComboBox";
-            this.satComboBox.Size = new System.Drawing.Size(85, 24);
+            this.satComboBox.Size = new System.Drawing.Size(68, 24);
             this.satComboBox.TabIndex = 3;
             // 
             // minutaComboBox
             // 
             this.minutaComboBox.FormattingEnabled = true;
-            this.minutaComboBox.Location = new System.Drawing.Point(213, 95);
+            this.minutaComboBox.Location = new System.Drawing.Point(230, 95);
             this.minutaComboBox.Margin = new System.Windows.Forms.Padding(4);
             this.minutaComboBox.Name = "minutaComboBox";
-            this.minutaComboBox.Size = new System.Drawing.Size(85, 24);
+            this.minutaComboBox.Size = new System.Drawing.Size(68, 24);
             this.minutaComboBox.TabIndex = 4;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.ForeColor = System.Drawing.Color.Black;
             this.label7.Location = new System.Drawing.Point(26, 9);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(272, 17);
@@ -181,7 +181,7 @@ namespace WareHome
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.ForeColor = System.Drawing.Color.Black;
             this.label8.Location = new System.Drawing.Point(70, 26);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(193, 17);
@@ -191,12 +191,12 @@ namespace WareHome
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.ForeColor = System.Drawing.Color.Red;
-            this.label9.Location = new System.Drawing.Point(5, 229);
+            this.label9.ForeColor = System.Drawing.Color.Green;
+            this.label9.Location = new System.Drawing.Point(3, 229);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(309, 17);
+            this.label9.Size = new System.Drawing.Size(313, 17);
             this.label9.TabIndex = 9;
-            this.label9.Text = "Za isključivanje podsjetnika, Dan postaviti na \"-\"";
+            this.label9.Text = "Za isključivanje podsjetnika, Dan postaviti na \"-\".";
             // 
             // RasporedForm
             // 
