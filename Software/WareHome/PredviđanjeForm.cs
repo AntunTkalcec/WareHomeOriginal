bc6859168433224491374dd75705c83cab6274dd
Karlo Habdija <khabdija@foi.hr>
2021-05-18 21:17:31 +0200

    PredviđanjeForm - dodan using Warehome.Models.ListaZaKupovinu (još nije korišten, dodan kao priprema za izradu ispisa)

--- Software/WareHome/PredviđanjeForm.cs
+++ Software/WareHome/PredviđanjeForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WareHome.Models.ListaZaKupovinu;
 using WareHome.Models.PredviđanjePotrošnje;
 using WareHome_Logic;
 
