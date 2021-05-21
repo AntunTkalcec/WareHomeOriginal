db6fc64cf9e00a58c42c669ccdc98f344d75a9c9
Kristijanstr <80409178+Kristijanstr@users.noreply.github.com>
2021-05-21 16:26:08 +0200

    Add files via upload
    Console app za slanje podsjetnika odlaska u kupovinu na e-mail korisnika

---
+++ Software/WareHomeEmailSender/Podsjetnik.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHomeEmailSender
+{
+    public class Podsjetnik
+    {
+        public string NazivNamirnice { get; set; }
+        public string Kolicina { get; set; }
+
+        public Podsjetnik()
+        {
+
+        }
+    }
+}
