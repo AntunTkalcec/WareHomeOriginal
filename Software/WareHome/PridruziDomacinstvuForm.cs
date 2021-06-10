a8a0af1a79c8787874643f120e09eba082b9f98a
Karlo Habdija <khabdija@foi.hr>
2021-06-10 02:15:31 +0200

    - u using dodani System.IO i System.Reflection kako bi se mogla pronaći .chm datoteka potrebna za prikazivanje pomoći klikom na F1 (uvijek će se nalaziti u root direktoriju gdje se nalazi i .exe datoteka) - implementiran event handler za klik na F1 koji otvara .chm datoteku na potrebnom poglavlju

--- Software/WareHome/PridruziDomacinstvuForm.cs
+++ Software/WareHome/PridruziDomacinstvuForm.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -79,6 +81,14 @@ namespace WareHome
             dataReader.Close();
             Database.Instance.Disconnect();
             return false;
+        }
+
+        private void PridruziDomacinstvuForm_HelpRequested(object sender, HelpEventArgs hlpevent)
+        {
+            string executableLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string helpLocation = Path.Combine(executableLocation, "helpfile.chm");
+            string helpfile = "File://" + helpLocation;
+            Help.ShowHelp(this, helpfile, HelpNavigator.KeywordIndex, "3.1. Prijava u postojece domacinstvo");
         }
     }
 }
